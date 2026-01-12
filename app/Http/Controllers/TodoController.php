5a58f065e382ee021e5c4e36f9c23ad5a8f45360
Rangga Firmansyah <ranggafirmansyah538@gmail.com>
2026-01-13 00:32:10 +0700

    Add API routing configuration to application bootstrap

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Todo::orderBy('created_at', 'desc')->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+        ]);
+
+        $todo = Todo::create([
+            'title' => $request->title,
+        ]);
+
+        return response()->json($todo, 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $todo = Todo::findOrFail($id);
+        $todo->delete();
+
+        return response()->json(['message' => 'Todo deleted successfully'], 200);
+    }
+}
